32c7db1beb91985d1b00c2d5b470a8137add67db
Kelvin gbolo CoDed <kelvingbolo98@gmail.com>
2022-10-28 21:31:24 -0700

    View now returns Basic html

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest, HttpResponse
+
 from lists.views import home_page 
 
 # Create your tests here.
@@ -7,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page(self) -> None:
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self) -> None:
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-DO list</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse(
+        '<html> <title>To-DO list</title>HelloWorld </html>'
+    )
+    
