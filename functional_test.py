64a86e8b727e57135cae10a0d672617cee894bef
Kelvin gbolo CoDed <kelvingbolo98@gmail.com>
2022-10-30 22:49:41 -0700

    Improved the code to obey DRY

--- functional_test.py
+++ functional_test.py
@@ -17,6 +17,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()  
 
+    def check_for_row_in_list_table(self, row_text: list ) -> None:
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self) -> None:
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -43,9 +48,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID , 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # there is still a text box inviting her to ad another item. She
         # enters "Use peacock feathers to make a fly"( Edith is very methodical)
@@ -56,10 +59,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
         # Edith wonders whether the site will remember her list. Then she sees
