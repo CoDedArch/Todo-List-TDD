d22c90c5f567de4a2638225e8305fe5bf27cee52
Kelvin gbolo CoDed <kelvingbolo98@gmail.com>
2022-11-01 10:16:09 -0700

    Just made our functional_test to be in its own app

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,13 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 #needs a unittest which is a module in the standard library
-import unittest
 import time
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self) -> None:
         self.options = Options()
@@ -25,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self) -> None:
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         
         # she notices the page title and header mention To-Do lists
         self.assertIn('To-Do', self.browser.title)
@@ -73,7 +73,3 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-        
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
