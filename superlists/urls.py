296960c32102ed2013b2669a035300d0c5d868c1
Kelvin gbolo CoDed <kelvingbolo98@gmail.com>
2022-11-08 21:18:41 -0800

    just added a new urls to lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path
-from lists import views
+from django.urls import re_path, include
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
-    re_path(r'^$', views.home_page, name='home_page' ),
+    re_path(r'^$', list_views.home_page, name='home_page' ),
+    re_path(r'^lists/', include(list_urls)),
     re_path(r'^lists/new$', views.new_list, name='new_list'),
     re_path(r'^lists/(\d+)/$', views.view_list, name= 'view_list'),
     re_path(r'^lists/(\d+)/add_item$', views.add_item, name= 'add_item')
